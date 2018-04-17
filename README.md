b712502ecbe3536d2329ddbc9a5fbc06f5772ef2
Eric <errose@amazon.com>
2018-04-17 19:03:02 -0400

    repo init

---
+++ README.md
@@ -0,0 +1,5 @@
+1.      Create a new Chime room
+2.      Create a new webhook (Gear icon> manage webhooks > new webhook) copy the webhook url
+3.      Launch the template (and provide Chime webhook url as arg)
+4.      Enable guardduty for the region and select General > Generate Sample Findings (and then wait about 5 minutes for CWE to do it's thing)
+5.      Profit!
---
+++ index.js
@@ -0,0 +1,27 @@
+const AWS = require("aws-sdk");
+const https = require("https");
+const webhook = process.env.CHIMEWEBHOOK.split('/').slice(-1);
+exports.handler = (event, context, callback) => {
+              console.log(event);
+              if (parseFloat(event.detail.severity) > parseFloat(process.env.SEVERITY)) {
+                              const req = https.request({
+                                                method: 'POST',
+                                                hostname: 'hooks.chime.aws',
+                                                path: `/incomingwebhooks/${webhook}`,
+                                                headers: {
+                                                                    'Content-Type': 'application/json'
+                                                                  }
+                                              }, res => {
+                                                callback(null, 'success');
+                                              });
+                              req.on('error', e => {
+                                                callback(`Error: ${e.message}`);
+                                              });
+                              req.write(JSON.stringify({
+                                                Content: `${event.detail.accountId} ${event.region} : ${event.detail.title}`
+                                              }));
+                              req.end();
+                            }
+              callback(null, event.detail.title);
+            };
+
